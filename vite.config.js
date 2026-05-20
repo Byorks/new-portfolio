1886da97c8c4bd4ff7426370c63d444a55c00957
Byork <vanessabyorksp@gmail.com>
2026-05-20 14:49:07 -0300

    :wrench: chore: better divided chunks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from 'rollup-plugin-visualizer'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,4 +19,21 @@ export default defineConfig({
       filename: "bundle-stats.html", // nome do arquivo gerado
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        // Agora sendo passado como uma função, como o Rolldown exige
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react") || id.includes("react-dom")) {
+              return "vendor";
+            }
+            if (id.includes("gsap")) {
+              return "gsap";
+            }
+          }
+        },
+      },
+    },
+  },
 });
